6b617f9f0bf9383379bb518ca2fb02a90419b965
fabianzielinski <fabz@o2.pl>
2019-04-02 16:12:59 +0200

    Add function in Celcius

--- js/script.js
+++ js/script.js
@@ -1,34 +1,89 @@
 'use strict';
 
 
+var buttonCes;
+var buttonFht;
+var text;
+var textInfo;
+var tempCes;
+var tempFht;
+var actTempCes;
+var actTempFht;
+var element;
+
 var outputMsg = document.getElementById('textMsg');
 var outputTmp = document.getElementById('textTemp');
 
-var buttonCes;
-var buttonFht;
+var buttonCes = document.getElementById('btnCes');
+var buttonFht = document.getElementById('btnFht');
 
 
-outputMsg.innerHTML = ' "Click the button! I acout degrees..." ' + '<br><br>' + outputMsg.innerHTML; 
 
-buttonCes = document.getElementById('btnCes');
-buttonFht = document.getElementById('btnFht');
+var printText = function( text,  element ) {
+	
+	element.innerHTML += text + '<br><br>'; 
+};
+
+var takeCelcius = function() {
+
+	tempCes = window.prompt('What is the temperature in degrees Celsius?');	
+	
+	if (isNaN(tempCes)) {outputMsg.innerHTML = " It's not a temperature !!! "};
+
+	return tempCes;
+};
+
+
+var takeFarenheit = function() {
+
+	tempFht = window.prompt('What is the temperature in degrees Farenheit?');	
+	
+	if (isNaN(tempFht)) {outputMsg.innerHTML = " It's not a temperature !!! "};
+
+	return tempFht;
+};
+
+var celcToFarenhFunction = function(tempCes){
+
+	actTempFht = ((tempCes*1.8)+32);
+
+	return printText(actTempFht, outputTmp);	
+};
+
+
+var celcToCelciusFunction = function(tempFht){
+	
+	actTempCes = ((tempFht-32)/1.8);
+
+	return printText(actTempCes, outputTmp);	
+};
+
+var tempInfo = function(tempCes) {
+
+
+	if ( tempCes >= 35) {textInfo = " It's to hot ............ !! ";}
+
+	else if (tempCes > 15) {textInfo = '" It is very nice ..."';}
+			 
+	else if (tempCes <= 5) {textInfo = '" Without a hat and scarf you will not get around "';}
+			
+	else {textInfo = '" Dress up warmer "';}
+
+	return printText(textInfo, outputMsg);
+
+};
+
+
 
 btnCes.addEventListener('click', function() {
 
-	var tempCes = window.prompt('What is the temperature in degrees Celsius?');	
+	takeCelcius();
+	celcToFarenhFunction(tempCes);
+	tempInfo(tempCes);
 
-	var tempFht = ((tempCes*1.8)+32);
+	// printText(actTempFht, outputTmp);
+	// printText(textInfo, outputMsg);
 
-		if (isNaN(tempCes)) {outputMsg.innerHTML = " It' not a temperature !!! " } 
-
-		else { if (tempCes > 15) {outputMsg.innerHTML = '" It is very nice ..."';}
-			 
-			   else if (tempCes <= 5) {outputMsg.innerHTML = '" Without a hat and scarf you will not get around "';}
-			
-			   else {outputMsg.innerHTML = '" Dress up warmer "';}
-	
-   		outputTmp.innerHTML = '" Temperature is '+ tempFht +'  F "';
-		} 
 })
 
 btnFht.addEventListener('click', function() {
@@ -48,3 +103,5 @@ btnFht.addEventListener('click', function() {
 			outputTmp.innerHTML = '" Temperature is '+ Math.round(tempCes) +'  C "';
 		} 
 })
+
+printText('Click the button! I acout degrees...', outputMsg);
